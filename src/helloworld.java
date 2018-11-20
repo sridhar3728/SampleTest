f89366aebf8bf4bee408754861a530e23264287d
sridhar3728 <45210862+sridhar3728@users.noreply.github.com>
2018-11-20 23:45:05 +0530

    adding first file for the project

---
+++ src/helloworld.java
@@ -0,0 +1,14 @@
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 100;
+		int b = 200;
+	
+		System.out.println("Hello world = " + a+b);
+
+	}
+
+}
