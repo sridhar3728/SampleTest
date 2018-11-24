675b44b3401321ba39f19e1f33b529d1cec4bc46
sridhar3728 <sridhar.a3728@gmail.com>
2018-11-25 01:29:55 +0530

    added substraction op

--- src/Subtract.java
+++ src/Subtract.java
@@ -1,1 +1,19 @@
 //Implement Subtraction
+
+public class Subtract {
+
+ private int a;
+ private int b;
+ 
+ public Subtract() { }
+
+ public setValues(int aVal, int bVal) {
+ 	a = aVal;
+ 	b = bVal;
+ }
+
+ public int afterSubtract() {
+ 	return a - b;
+ }
+
+}
