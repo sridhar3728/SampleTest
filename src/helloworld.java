e0dcde2fc76bee945888e1d70aed22e09c8e7778
sridhar3728 <45210862+sridhar3728@users.noreply.github.com>
2018-11-25 00:47:55 +0530

    Update helloworld.java
    added variable C

--- src/helloworld.java
+++ src/helloworld.java
@@ -3,11 +3,12 @@ public class helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		System.out.println("Hello world...")
 		int a = 100;
 		int b = 200;
+		int c = 300
 	
-		System.out.println("Hello world = " + a+b);
+		System.out.println("Value = " + a+b-c);
 
 	}
 
