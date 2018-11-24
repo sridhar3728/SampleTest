0f95b0699df1b54ce89a477a5a96b0c3dabc03e1
sridhar3728 <sridhar.a3728@gmail.com>
2018-11-24 23:50:11 +0530

    addition operation

---
+++ src/Add.java
@@ -0,0 +1,18 @@
+
+public class Add {
+
+ private int a;
+ private int b;
+
+ public void Add() {}
+ 
+ public void setValues(int aVal, int bVal) {
+ 	a = aVal;
+ 	b = bVal;
+ }
+
+ public int afterAddition() {
+	return a + b;
+ }	
+
+}
--- src/helloworld.java
+++ src/helloworld.java
@@ -3,12 +3,14 @@ public class helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world...")
-		int a = 100;
-		int b = 200;
-		int c = 300
-	
-		System.out.println("Value = " + a+b-c);
+		System.out.println("Hello world...");
+
+		Add a = new Add();
+                a.setValues(10, 20);
+                
+                int result = a.afterAddition();
+
+		System.out.println("Result = " result);
 
 	}
 
